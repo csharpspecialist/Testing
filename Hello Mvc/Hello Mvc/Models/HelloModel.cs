cc137fce65b28f5b43ad43c18dd418ea8d54e059
csharpspecialist <netspec1@hotmail.com>
2016-10-02 06:02:26 -0400

    started the first MVC project w Bob Tabor's MVC video course

---
+++ Hello Mvc/Hello Mvc/Models/HelloModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Hello_Mvc.Models
+{
+    public class HelloModel
+    {
+        public static List<Car> GetCars()
+        {
+            List<Car> cars = new List<Car>
+            {
+                new Car {Id = 1, Make = "Ford", Model= "Flex" },
+                new Car {Id = 2, Make = "Dodge", Model= "Ram 1500" },
+                new Car {Id = 3, Make = "Pontiac", Model= "G8" }
+                
+            };
+            return cars;
+        }
+
+    }
+
+
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+    }
+
+}
