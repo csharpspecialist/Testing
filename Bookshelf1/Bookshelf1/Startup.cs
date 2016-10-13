9f659837cc34742962c19709a1a638929372d79f
csharpspecialist <netspec1@hotmail.com>
2016-10-13 02:18:50 -0400

    just pushing the files up for safe keeping

---
+++ Bookshelf1/Bookshelf1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Bookshelf1.Startup))]
+namespace Bookshelf1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Scoffolding/Scoffolding/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Scoffolding.MvcApplication" Language="C#" %>
